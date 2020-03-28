dc15faa8b718cfd8195a22dc1b20023e7f668e67
gasperlf <gasper_lf@hotmail.com>
2020-03-28 15:26:16 +0100

    add changes to the model project

---
+++ api-app/test/java/co/com/javadeveloperscolombia/api/app/Application.java
@@ -0,0 +1,13 @@
+package co.com.javadeveloperscolombia.api.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
